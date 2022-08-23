47d04fcf6379e8fc3a7cde612a88e26f60766aec
Anilkumar <Anilkumar@DESKTOP-L1TPG05>
2022-08-23 21:38:15 +0530

    first release

---
+++ Automation/src/Generic/Framework_Const.java
@@ -0,0 +1,11 @@
+package Generic;
+
+public interface Framework_Const {
+	String geckokey="webdriver.gecko.driver";
+	String geckovalue="./software/geckodriver.exe";
+	
+	String URL="https://wwww.facebook.com";
+}
+
+
+
