f6903a5e17c2e50c92ade1a56198cf8b7e989103
Anilkumar <Anilkumar@DESKTOP-L1TPG05>
2022-08-24 11:32:04 +0530

    1st release

---
+++ Automation/src/Framework/Base_Class.java
@@ -0,0 +1,41 @@
+package Framework;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+public class Base_Class {
+	WebDriver driver;
+	@BeforeMethod
+	public void openapp()
+	{
+		System.setProperty("webdriver.gecko.driver", "./software/geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+	}
+	
+	@AfterMethod
+	public void closeapp(ITestResult res)
+	{
+		if(ITestResult.FAILURE==res.getStatus())
+		{
+		
+			driver.quit();
+			
+		}
+		{
+			
+		}
+	}
+	
+
+	
+	
+
+}
